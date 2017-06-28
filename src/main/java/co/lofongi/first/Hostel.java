387445ffd3c2d57e4d6eb7721ed08d5db9ac21f6
Andriy Yahello <andrey.lv.west@gmail.com>
2017-06-28 10:16:50 +0300

    autowire hostel byName

---
+++ src/main/java/co/lofongi/first/Hostel.java
@@ -0,0 +1,24 @@
+package co.lofongi.first;
+
+public class Hostel {
+	private String hostelBName;
+	private String city;
+	public Hostel() {}
+	
+	public String getHostelBName() {
+		return hostelBName;
+	}
+	public void setHostelBName(String hostelBName) {
+		this.hostelBName = hostelBName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+	
+}
