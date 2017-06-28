50b0d4bbbf3593103811e198c7b7eab40f173c15
Andriy Yahello <andrey.lv.west@gmail.com>
2017-06-28 21:50:14 +0300

    creating beans with @Conponent annotaion

---
+++ src/main/java/co/lofongi/third/Excited.java
@@ -0,0 +1,15 @@
+package co.lofongi.third;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Excited {
+	
+	//in test we write excited for bean
+
+		public void WoWMan(){
+			System.out.println("I'm unstoppable!");
+		}
+
+
+}
